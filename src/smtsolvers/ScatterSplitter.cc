5d6079c69ccf2df8cb4ac155840740c2c8e15b59
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-03-10 14:51:13 +0100

    ScatterSplitter: remove repeated includes

--- src/smtsolvers/ScatterSplitter.cc
+++ src/smtsolvers/ScatterSplitter.cc
@@ -10,8 +10,6 @@
 #include "SystemQueries.h"
 #include "ReportUtils.h"
 
-#include "SystemQueries.h"
-#include "ReportUtils.h"
 
 
 ScatterSplitter::ScatterSplitter(SMTConfig & c, THandler & t, Channel & ch)
